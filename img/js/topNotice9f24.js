4d0f9f8524803a726e7a37a7e2521d4e1becc324
mhdsaifullah <muhammadsaifullah@don.co.uk>
2024-11-15 12:00:51 +0000

    Update topNotice9f24.js

--- img/js/topNotice9f24.js
+++ img/js/topNotice9f24.js
@@ -36,7 +36,20 @@ var TopNotice = new (function () {
 
     }
     this.ShowNotice = ShowNotice;
+    /**
+     * 获取并展示通知
+     * @param {any} position
+     */
+    this.Notice = function (position) {
+        that.GetNoticeData(position, function (data) {
+            that.ShowNotice(data)
+        });
+    }
 
+
+    this.GetNoticeData = function (position, callback) {
+        $.get('/Home/GetWebNotice', { position: position }, callback)
+    }
 
 
 });
