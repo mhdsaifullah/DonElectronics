89a244a961fd66ebc1d91d2e8a061b6e5897b7a6
mhdsaifullah <muhammadsaifullah@don.co.uk>
2025-01-07 13:44:11 +0000

    Update script.js

--- octAPI-BK/script.js
+++ octAPI-BK/script.js
@@ -1,4 +1,4 @@
-import config from './config.js';
+import config from 'config.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('search-button');
